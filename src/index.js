2663ae2e3fb842319f4d51b5c049c3ddc06c07eb
Alexander <46690347+zanchess@users.noreply.github.com>
2020-03-05 23:08:35 +0300

    feat: end brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 0) {
+    for (let i = 0; i < bracketsConfig.length;) {
+    	let bracket = bracketsConfig[i].join('')
+  		if (str.includes(bracket)) {
+        str = str.replace(bracket, '');
+        i = 0;
+    	} else{
+        i++;
+    	}
+    
+		}  
+		if (str) {
+  		return false;
+		}else{
+    	return true;
+		}
+	} else{
+    return false;
+	}
 }
